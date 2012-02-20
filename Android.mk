41cbf52f712af9b0dd57e4c7659d437df7912ccd
Jehan Monnier <jehan.monnier@linphone.org>
2012-02-20 10:52:20 +0100

    add Android.mk files

---
+++ Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libbcg729
+
+
+LOCAL_SRC_FILES =       src/LP2LSPConversion.c \
+                        src/LPSynthesisFilter.c \
+                        src/LSPQuantization.c \
+                        src/adaptativeCodebookSearch.c \
+                        src/codebooks.c \
+                        src/computeAdaptativeCodebookGain.c \
+                        src/computeLP.c \
+                        src/computeWeightedSpeech.c \
+                        src/decodeAdaptativeCodeVector.c \
+                        src/decodeFixedCodeVector.c \
+                        src/decodeGains.c \
+                        src/decodeLSP.c \
+                        src/decoder.c \
+                        src/encoder.c \
+                        src/findOpenLoopPitchDelay.c \
+                        src/fixedCodebookSearch.c \
+                        src/gainQuantization.c \
+                        src/interpolateqLSP.c \
+                        src/postFilter.c \
+                        src/postProcessing.c \
+                        src/preProcessing.c \
+                        src/qLSP2LP.c \
+                        src/utils.c
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include
+
+include $(BUILD_SHARED_LIBRARY)
+
+
---
+++ msbcg729/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libmsbcg729
+
+
+LOCAL_SRC_FILES = bcg729_enc.c bcg729_dec.c 
+
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/../../linphone/oRTP/include \
+	$(LOCAL_PATH)/../../linphone/mediastreamer2/include \
+	$(LOCAL_PATH)/../include
+LOCAL_SHARED_LIBRARIES = libbcg729
+include $(BUILD_STATIC_LIBRARY)
+
+
